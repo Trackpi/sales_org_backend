8676df5b8ff1f321b8e2f57290b41543d7c8f4a9
Abhishek Pavithran <61498092+abhishek-475@users.noreply.github.com>
2025-01-04 10:20:09 +0530

    Update adminRouter.js
    admin-login

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,22 +1,15 @@
 const express = require("express");
-const verifyJwt = require("../middlewares/jwtMiddleware"); // Import JWT middleware
+const verifyJwt = require("../middlewares/jwtMiddleware"); 
 
-const adminController = require("../controllers/adminController"); // Adjust the path
+const adminController = require("../controllers/adminController"); 
 const router = express.Router();
 
 
-// Route for creating a new admin
-router.post("/create", adminController.createAdmin);
-
-// Route for logging in as admin
 router.post("/login", adminController.adminlogin);
 
-
-// Route for updating an admin by ID
 router.put("/:id",verifyJwt,adminController.updateAdmin);
 
-// Route for deleting an admin by ID
-router.delete("/:id",verifyJwt, adminController.deleteAdmin);
+
 
 
 module.exports = router;
