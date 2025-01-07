092623a82143555d412661542a2b7222de26c260
Najil Rahman PM <sunaidnk1.gmail.com>
2025-01-07 11:24:52 +0530

    added admin history

--- models/adminmodel.js
+++ models/adminmodel.js
@@ -6,10 +6,19 @@ const adminSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
+  email: {
+    type: String,
+    unique: true,
+  },
   password: {
     type: String,
     required: true,
   },
+  date: {
+    type: Date,
+    default:Date.now()
+  }
+  
 });
 
 const adminModel = new mongoose.model("admins", adminSchema);
