a6acad980c6e898b9015b57312029a3479acc96c
akashcp22 <akashcp22@gmail.com>
2025-01-13 21:20:11 +0530

    admin management(model,controller,routes)

--- models/adminManagementModel.js
+++ models/adminManagementModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const adminSchema = new mongoose.Schema({
+  fullname: {
+    type: String,
+    required: true,
+  },
   username: {
     type: String,
     required: true,
@@ -15,10 +19,16 @@ const adminSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  isActive: {
-    type: Boolean,
-    default: true,
+  role: {
+    type: String,
+    required: true,
+    enum: ['superadmin', 'admin'], // Define possible roles
+    default: 'admin',
+  },
+  profilePic: {
+    type: String, // URL or file path to the profile picture
+    default: null,
   },
 }, { timestamps: true });
 
-module.exports = mongoose.model('admins', adminSchema);
+module.exports = mongoose.model('Admin', adminSchema);
