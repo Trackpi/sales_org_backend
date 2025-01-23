38cfa71545a336671b4bd3f1971ea8a6f32dee2f
Najil Rahman PM <sunaidnk1.gmail.com>
2025-01-23 17:05:50 +0530

    added specific search and serch by company id in faq . added history in every actions

--- routes/faqRoute.js
+++ routes/faqRoute.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const faqController = require('../controllers/faqController');
+const verifyJwt = require('../middlewares/jwtMiddleware');
 
-router.post('/faqs', faqController.createFaq);
-router.get('/faqs', faqController.getFaqs);
-router.put('/faqs/:id', faqController.updateFaq);
-router.delete('/faqs/:id', faqController.deleteFaq);
+// FAQ Routes
+router.post('/faqs',verifyJwt, faqController.createFaq); // Create FAQ
+router.get('/faqs',verifyJwt, faqController.getFaqs); // Get FAQs with pagination and optional company filter
+router.get('/faqs/all',verifyJwt, faqController.getAllFaqs); // Get all FAQs without pagination
+router.get('/faqs/company/:companyId',verifyJwt, faqController.getFaqsByCompany); // Get FAQs by company
+router.get('/faqs/:id',verifyJwt, faqController.getFaqById); // Get a specific FAQ by ID
+router.put('/faqs/:id',verifyJwt, faqController.updateFaq); // Update FAQ by ID
+router.delete('/faqs/:id',verifyJwt, faqController.deleteFaq); // Delete FAQ by ID
 
 module.exports = router;
